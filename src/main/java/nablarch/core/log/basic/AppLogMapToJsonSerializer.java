6ad45d74ce84e3faf1c17fdb0e19386450223a74
opengl-8080 <opengl-8080@users.noreply.github.com>
2021-12-10 19:21:36 +0900

    refactor: SonarQube対応 複雑すぎると判定されたメソッドのリファクタリング

--- src/main/java/nablarch/core/log/basic/AppLogMapToJsonSerializer.java
+++ src/main/java/nablarch/core/log/basic/AppLogMapToJsonSerializer.java
@@ -31,37 +31,73 @@ public class AppLogMapToJsonSerializer extends MapToJsonSerializer {
     @Override
     public void serialize(Writer writer, Object value) throws IOException {
         Map<?, ?> map = (Map<?, ?>) value;
-        boolean isFirst = true;
+        boolean first = true;
         writer.append(BEGIN_OBJECT);
         for (Map.Entry<?, ?> member : map.entrySet()) {
-            Object memberName = member.getKey();
-            if (memberName != null && memberNameSerializer.isTarget(memberName.getClass())) {
-                Object memberValue = member.getValue();
-                if (memberValue != null || !isIgnoreNullValueMember) {
-                    if (memberValue instanceof RawJsonObjectMembers) {
-                        RawJsonObjectMembers rawMembers = (RawJsonObjectMembers) memberValue;
-                        if (!rawMembers.isJsonWhitespace()) {
-                            if (!isFirst) {
-                                writer.append(VALUE_SEPARATOR);
-                            } else {
-                                isFirst = false;
-                            }
-                        }
-                        writer.append(rawMembers.getRawJsonText());
-                    } else {
-                        if (!isFirst) {
-                            writer.append(VALUE_SEPARATOR);
-                        } else {
-                            isFirst = false;
-                        }
-                        memberNameSerializer.serialize(writer, memberName);
-                        writer.append(NAME_SEPARATOR);
-                        manager.getSerializer(memberValue).serialize(writer, memberValue);
-                    }
-                }
+            if (isSkip(member)) {
+                continue;
             }
+
+            if (!first) {
+                writer.append(VALUE_SEPARATOR);
+            }
+
+            Object memberValue = member.getValue();
+
+            if (memberValue instanceof RawJsonObjectMembers) {
+                RawJsonObjectMembers rawMembers = (RawJsonObjectMembers) memberValue;
+                writer.append(rawMembers.getRawJsonText());
+            } else {
+                Object memberName = member.getKey();
+                memberNameSerializer.serialize(writer, memberName);
+                writer.append(NAME_SEPARATOR);
+                manager.getSerializer(memberValue).serialize(writer, memberValue);
+            }
+
+            first = false;
         }
         writer.append(END_OBJECT);
     }
 
+    /**
+     * 指定されたメンバーが出力の条件を満たしていないことを判定する。
+     * @param member 判定対象のメンバー
+     * @return 出力の条件を満たしていない場合は true
+     */
+    private boolean isSkip(Map.Entry<?, ?> member) {
+        return isNotSupportedMemberName(member.getKey())
+                || isNotSupportedMemberValue(member.getValue())
+                || isEmpty(member.getValue());
+    }
+
+    /**
+     * メンバーの名前が出力サポート対象か判定する。
+     * @param memberName メンバーの名前
+     * @return 出力サポート対象の場合は true
+     */
+    private boolean isNotSupportedMemberName(Object memberName) {
+        return memberName == null || !memberNameSerializer.isTarget(memberName.getClass());
+    }
+
+    /**
+     * メンバーの値が出力がサポートされていない値かどうか判定する。
+     * @param memberValue メンバーの値
+     * @return 出力がサポートされていない値の場合は true
+     */
+    private boolean isNotSupportedMemberValue(Object memberValue) {
+        return memberValue == null && isIgnoreNullValueMember;
+    }
+
+    /**
+     * メンバーの値が空で出力が不要かどうかを判定する。
+     * @param memberValue メンバーの値
+     * @return 空で出力不要の場合は true
+     */
+    private boolean isEmpty(Object memberValue) {
+        if (memberValue instanceof RawJsonObjectMembers) {
+            return ((RawJsonObjectMembers) memberValue).isJsonWhitespace();
+        } else {
+            return false;
+        }
+    }
 }
