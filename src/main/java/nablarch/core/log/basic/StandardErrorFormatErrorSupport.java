18817530b09f20cc914ca5d9bfb4ec24ee0c61f3
opengl-8080 <opengl-8080@users.noreply.github.com>
2021-12-16 15:48:28 +0900

    fix: SonarQube警告対応 問題ないものなので SuppressWarnings で無視するように修正

--- src/main/java/nablarch/core/log/basic/StandardErrorFormatErrorSupport.java
+++ src/main/java/nablarch/core/log/basic/StandardErrorFormatErrorSupport.java
@@ -11,6 +11,8 @@ public class StandardErrorFormatErrorSupport implements FormatErrorSupport {
      * {@inheritDoc}
      */
     @Override
+    // ログ出力できないときに最終手段として標準エラー出力に書き出すためのクラスのため、この警告は無視して問題ない
+    @SuppressWarnings("squid:S106")
     public void outputFormatError(String message) {
         System.err.println(message);
     }
