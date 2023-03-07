a344bf3701d61c12a604c1eb71671e8873083571
Tacitus <tacitusxo@gmail.com>
2023-03-07 13:05:01 +0900

    test: 不要なFixedDateRotatePolicyの利用を削除

--- src/test/java/nablarch/core/log/basic/DateRotatePolicyTest.java
+++ src/test/java/nablarch/core/log/basic/DateRotatePolicyTest.java
@@ -128,7 +128,7 @@ public class DateRotatePolicyTest {
         // ファイル更新時刻の設定
         logFile.setLastModified(textToDate("2017-12-31 10:10:10.000").getTime());
 
-        DateRotatePolicy policy = new DateRotatePolicyForTest(textToDate("2018-01-01 10:10:10.000"));
+        DateRotatePolicy policy = new DateRotatePolicy();
         policy.initialize(objectSettings);
 
         boolean actual = policy.needsRotate("abcdeabcde", ignored);
